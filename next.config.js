53040c2ad742602717a5bc7f612c6d71498ec6fc
Shinnφ <166776356+Shinniam@users.noreply.github.com>
2025-04-10 21:06:39 +0900

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+};
